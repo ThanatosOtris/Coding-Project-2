4e1ea0f8caa559258786b17f734d4565762cffaf
ThanatosOtris <148538223+ThanatosOtris@users.noreply.github.com>
2025-01-23 23:54:52 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tier_inventar",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Dein Name",
+    author_email="deine@email.com",
+    description="Ein einfaches Tierinventar-System",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/deinname/tier_inventar",
+)
